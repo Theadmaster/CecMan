24434f3fadbc14a83d63c16666ef8dde797e4c77
Theadmaster <601728814@qq.com>
2021-02-26 23:15:11 +0800

    request

---
+++ utils/request.js
@@ -0,0 +1,29 @@
+// request.js
+const BASE_URL = 'http://121.196.159.48'
+export const myRequest = (options) => {
+	return new Promise((resolve, reject) => {
+		uni.showLoading({
+			title:'加载中'
+		})
+		uni.request({
+			header: options.header || {},
+			url: BASE_URL + options.url,
+			method: options.method || 'GET',
+			data: options.data || {},
+			success: (res) => {
+				// console.log(res)
+				uni.hideLoading()
+				resolve(res)
+
+			},
+			fail: (err) => {
+				// console.log(err)
+				uni.hideLoading()
+				uni.showToast({
+					title: '请求失败'
+				})
+				reject(err)
+			}
+		})
+	})
+}
